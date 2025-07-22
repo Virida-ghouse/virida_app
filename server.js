881b46f43b204ef2eab337b1b2a99f7e247722a5
Christophe Ganou <ganou.christophe@gmail.com>
2025-07-22 17:54:43 +0200

    Conversion de server.js en module ES6

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
-const express = require('express');
-const path = require('path');
-const fs = require('fs');
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import fs from 'fs';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
 const PORT = process.env.PORT || 8080;
 
